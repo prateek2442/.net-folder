68b4990cdacbce3bf3c0cf6d0d2b16174cfebac2
Prateek singh <devsin2442@gmail.com>
2026-03-25 09:25:30 +0530

    some more codes

---
+++ CAMPUS_FIX/Controllers/GeneralController.cs
@@ -0,0 +1,142 @@
+﻿using CAMPUS_FIX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAMPUS_FIX.Controllers
+{
+    public class GeneralController : Controller
+    {
+
+        ComplaintdbEntities db = new ComplaintdbEntities();
+
+        // GET: General
+        public ActionResult Index()
+        {
+            // to show notification
+
+            List<Notification_Master> lst=db.Notification_Master.OrderByDescending(x => x.NotiId).Take(3).ToList();
+            return View(lst);
+        }
+
+        public ActionResult TrackPage()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult ContactUs(Enquiry_Master cu)
+        {
+            string msg = "";
+            try
+            {
+                cu.EnquiryDate = DateTime.Now;
+                db.Enquiry_Master.Add(cu);
+                db.SaveChanges();
+                msg = "thanks for your sending your contact!. We will contact you soon!";
+            }
+            catch
+            {
+                msg = "Sorry! some Technical issue occured";
+            }
+
+            TempData["Message"] = msg;
+            return RedirectToAction("response");
+            
+        }
+        public ActionResult ReportComplaint()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReportComplaint(Complaint_Master cm)
+        {
+            string msg = "";
+            try
+            {
+                cm.ReportDate = DateTime.Now;
+                db.Complaint_Master.Add(cm);
+                db.SaveChanges();
+                msg = "thanks for your Enquiry!. We will contact you soon!";
+                
+            }
+            catch
+            {
+                msg = "Sorry! some Technical issue occured";
+            }
+            TempData["message"] = msg;
+            return RedirectToAction("ReportComplaint");
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult DeveloperPage ()
+        {
+            return View();
+        }
+
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AdminLogin(login_Master lm)
+        {
+            login_Master lmdb = db.login_Master.SingleOrDefault(x => x.AdminId == lm.AdminId && x.Password == lm.Password);
+            if(lmdb != null)
+            {
+                Session["Aid"]= lmdb.AdminId;
+                return RedirectToAction("Index","Admin");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+            }
+            return View();
+        }
+
+        //Save Enquiry
+
+        public ActionResult SaveEnquiry(Enquiry_Master em)
+        {
+            string msg = "";
+            try
+            {
+                em.EnquiryDate = DateTime.Now;
+                db.Enquiry_Master.Add(em);
+                db.SaveChanges();
+                msg = "thanks for your Enquiry!. We will contact you soon!";
+
+                mailer m = new mailer();
+                em.Message = "thanks for your Enquiry.We will contact you soon.";
+                m.SendMyEmail(em.Email, em.Subject, em.Message);
+            }
+            catch
+            {
+                msg = "Sorry! some Technical issue occured" ;
+            }
+
+            TempData["Message"] = msg;
+            return RedirectToAction("response");
+        }
+
+        public ActionResult response()
+        {
+            return View();
+        }
+    }
+}
---
+++ CAMPUS_FIX/Models/mailer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace CAMPUS_FIX.Models
+{
+    public class mailer
+    {
+        // public mailer() { }
+        string MyEmailMail = "";
+        string MyMailAppPassword = "";
+        public bool SendMyEmail(string sendTo, string Subject, string message)
+        {
+            try
+            {
+                // 1- perform setting of msg
+                MailMessage msg = new MailMessage();
+                MailAddress mafrom = new MailAddress(MyEmailMail);
+                msg.From = mafrom;
+                msg.Subject = Subject;
+                msg.To.Add(sendTo);
+                msg.Body = message;
+                // 2- create and set smtp protocol
+                SmtpClient client = new SmtpClient();
+                client.Host = "localhost";
+                client.Port = 587;
+                client.EnableSsl = true;
+                //client.Send(msg);
+                NetworkCredential nc = new NetworkCredential(MyEmailMail, MyMailAppPassword);
+                client.Credentials = nc;
+
+
+                // 3- send email
+                client.Send(msg);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
